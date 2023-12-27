f8524909f89bf3bfc1ef51d43071688a8a55c605
Leandro Rodrigues Jacome <leandro.jacome@capgemini.com>
2023-12-27 17:21:21 -0300

    Atualiza o arquivo EnableSefazCucumber.java e o arquivo pom.xml. No pom.xml, as alterações incluem a atualização da versão do cucumber-java para 7.0.0 e a adição da dependência do cucumber-spring. Além disso, a versão do jjwt foi substituída pela variável ${jjwt.version} e foram adicionadas as dependências do cucumber-java e cucumber-spring.
    sefaz-al/ti/arquitetura/arquitetura-info#1055

---
+++ sfz-cucumber-spring/src/main/java/br/gov/al/sefaz/architecture/config/EnableSefazCucumber.java
@@ -0,0 +1,13 @@
+package br.gov.al.sefaz.architecture.config;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(CucumberConfig.class)
+public @interface EnableSefazCucumber {
+}
