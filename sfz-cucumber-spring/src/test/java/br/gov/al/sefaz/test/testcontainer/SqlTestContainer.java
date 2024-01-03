53661432da3d9b492a56c1673c2d9b44b00b7ac4
Leandro Rodrigues Jacome <leandro.jacome@capgemini.com>
2024-01-03 16:27:27 -0300

    Adiciona uma dependência ao arquivo pom.xml para utilizar o Testcontainers e modifica a classe SqlTestContainer importando o GenericContainer e alterando o tipo de retorno do método getTestContainer de JdbcDatabaseContainer para GenericContainer. Também adiciona o arquivo DbicSqlTestContainer.java.
    sefaz-al/ti/arquitetura/arquitetura-info#1056

--- sfz-cucumber-spring/src/test/java/br/gov/al/sefaz/test/testcontainer/SqlTestContainer.java
+++ sfz-cucumber-spring/src/test/java/br/gov/al/sefaz/test/testcontainer/SqlTestContainer.java
@@ -2,8 +2,9 @@ package br.gov.al.sefaz.test.testcontainer;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 
 public interface SqlTestContainer extends InitializingBean, DisposableBean {
-    JdbcDatabaseContainer<?> getTestContainer();
+    GenericContainer<?> getTestContainer();
 }
